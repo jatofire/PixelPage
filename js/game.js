b2b2fe1965202d246232022dad471e107d0d28de
jatofire <chris>
2017-01-16 17:55:45 +1100

    Working on fill

--- js/game.js
+++ js/game.js
@@ -14,48 +14,35 @@ function Map(mapWidth, mapHeight, tileWidth, tileHeight) {
     }
   }
 
+
+  for(i= 0; i < this.mapWidth * this.tileWidth; i += this.tileWidth) {
+    for(j = 0; j < this.mapHeight * this.tileHeight; j += this.tileHeight) {
+      var tile = new Tile(i, j, currentColour,  currentColour + " Tile");
+      tile.visible = false;
+      this.tiles.push(tile);
+    }
+  }
+
+
+
 };
 
 var map;
 var preview;
-var currentColour = "";
+var currentColour = "grey";
 var action = "draw";
 var mouseDown = false;
 
-
+var previousTile;
 
 
 function StartGame() {
 
-  var blueColour = document.getElementById("blueColourID");
-  blueColour.addEventListener('click', function() {
-    currentColour = "blue";
-  });
-  var redColour = document.getElementById("redColourID");
-  redColour.addEventListener('click', function() {
-    currentColour = "red";
-  });
-  var greenColour = document.getElementById("greenColourID");
-  greenColour.addEventListener('click', function() {
-    currentColour = "green";
-  });
-  var yellowColour = document.getElementById("yellowColourID");
-  yellowColour.addEventListener('click', function() {
-    currentColour = "yellow";
-  });
-  var blackColour = document.getElementById("blackColourID");
-  blackColour.addEventListener('click', function() {
-    currentColour = "black";
-  });
-  var whiteColour = document.getElementById("whiteColourID");
-  whiteColour.addEventListener('click', function() {
-    currentColour = "white";
-  });
+  
 
   var gameContainer = document.getElementById("game");
   gameContainer.addEventListener('mousedown', function(e) {
-    mouseDown = true;
-   
+      HandleClick(e);
   });
 
     gameContainer.addEventListener('mouseup', function(e) {
@@ -64,9 +51,7 @@ function StartGame() {
   });
       
   gameContainer.addEventListener('mousemove', function(e) {
-    if(mouseDown) {
-     HandleClick(e);
-    }
+     HandleMoved(e)
   });
 
   preview = document.getElementById("previewimage");
@@ -79,6 +64,8 @@ function StartGame() {
   currentColour = "black";
 
 }
+
+
    
 var gameArea = {
   canvas : document.createElement("canvas"),
@@ -103,30 +90,67 @@ function randomMap() {
 
 }
 
+function SetCurrentColour(colour) {
+  var previewColour = document.getElementById("currentColourPreview");
+  previewColour.style.backgroundColor = colour;
+  currentColour = colour;
+}
 
-function HandleClick(e) {
+function HandleMoved(e) {
+  if(mouseDown === true) {
+
   var t = getRelativeCoords(e);
+  
   if(t.x < 0 || t.x > gameArea.canvas.width || t.y < 0 || t.y > gameArea.canvas.height) {
     mouseDown = false;
     return;
   }
   
+  console.log("Action: " + this.action);
+  var tile = tileAtPos(t.x, t.y);
+  var index = map.tiles.indexOf(tile);
+
+  if(previousTile == index) {
+  } else {
+
+    if(this.action === "draw") {
+      console.log("Draw");
+      DrawTile(tile);
+    }
+    if(this.action === "erase") {
+      if(tile != null) {
+        console.log("Erase");
+        tile.visible = false;
+      }
+    }
+  }
+
+  previousTile = index;
+
+  }
+}
+
+
+function HandleClick(e) {
+  
+  var t = getRelativeCoords(e);
   var tile = tileAtPos(t.x, t.y);
   
   if(this.action === "draw") {
-    var x = t.x - (t.x % map.tileWidth);
-    var y = t.y - (t.y % map.tileHeight);
-    map.tiles.push(new Tile(x, y, currentColour,  currentColour + " Tile"));
-    return;
-  } else if(this.action == "erase") {
-    if(tile) {
-    tile.colour = "red";
-    tile.visible = false;
+      console.log("Draw");
+      DrawTile(tile);
+      mouseDown = true;
     }
-  }
-  
-
-    //preview.src = gameArea.canvas.toDataURL("image/png");
+    if(this.action === "erase") {
+      if(tile != null) {
+        console.log("Erase");
+        tile.visible = false;
+        mouseDown = true;
+      }
+    }
+    if(this.action === "fill") {
+      Fill(tile);
+    }
 
 }
 
@@ -135,7 +159,102 @@ function SetAction(act) {
   console.log("Action changed to: " + this.action);
 }
 
+function DrawTile(tile) {
+  tile.colour = currentColour;
+  tile.visible = true;
+}
 
+function MakeImage() {
+ preview.src = gameArea.canvas.toDataURL("image/png");
+}
+
+function Fill(tile) {
+  
+  var tilesToCheck = [];
+  var tilesChecked = [];
+  var checkingTiles = true;
+  tilesToCheck.push(tile);
+
+ 
+ 
+  //check top
+ 
+  while(checkingTiles === true) {
+    
+    
+    //console.log("Tiles to add length: " + tilesToAdd.length);
+
+    var tilesToAdd = CheckSurrounding(tilesToCheck[0]);
+    console.log("Tiles to add length: " + tilesToAdd.length);
+
+
+    if(tilesToAdd == null) {} else {
+      for(i= 0; i < tilesToAdd.length; i +=1 ) {
+        console.log("Checking tile: " + this.tilesToAdd[i].y);
+      }
+    }
+  
+    if(tilesToAdd == null) {} else {
+
+      for(i = 0; i < tilesToAdd.length; i += 1) {
+        tilesToCheck.push(tilesToAdd[i]);
+      }
+    }
+
+    var indexToAdd = map.tiles.indexOf(tilesToCheck[0]);
+
+    tilesChecked.push(indexToAdd);
+    tilesToCheck.splice(0, 1);
+    console.log("Tiles to check length: " + tilesToCheck.length);
+    console.log("Tiles checked length: " + tilesChecked.length);
+    if(tilesToCheck.length === 0) { checkingTiles = false }; 
+  }
+
+
+
+
+  for( j = 0; j < tilesChecked.length; j += 1) {
+    console.log("Checked: " + tilesChecked[j]);
+    if(tilesChecked[j] != -1) {
+      map.tiles[tilesChecked[j]].visible = true;
+      map.tiles[tilesChecked[j]].colour = "red";
+    }
+  }
+
+
+
+}
+
+function CheckSurrounding(tile) {
+  
+  var tiles = [];
+ console.log("Checking tile in surrounding: " + tile.y);
+  //check top
+  if(tile.y - 10 > 0) {
+    var topTile = tileAtPos(tile.x + 10, tile.y - 10);
+    var topIndex = map.tiles.indexOf(topTile);
+    tiles.push(this.topIndex);
+  }
+
+  //check bottom
+ if(tile.y + 10 < map.mapHeight * map.tileHeight) {
+    var bottomTile = tileAtPos(tile.x + map.tileHeight + 10, tile.y + 10);
+    var bottomIndex = map.tiles.indexOf(bottomTile);
+    tiles.push(this.bottomIndex);
+  }
+
+  // check right
+
+
+  /// check left
+
+  
+  if(tiles.length > 0) {
+    return tiles;
+  } else {
+    return null;
+  }
+}
 
 
 function tileAtPos(x, y){
